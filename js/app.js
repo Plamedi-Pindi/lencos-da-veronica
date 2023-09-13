1e07b5c09a26b306f29ecc333acb667c5523a296
Plamedi Pindi <plamedipindi77@gmail.com>
2023-09-13 16:46:35 +0100

    Atualizacao

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// ############ COLOR DEFINITION ############
+const primaryColor = '#EE8A9B'
+
+// ############ VARIABLE DEFINITION ###########
+
 const card = document.getElementsByClassName('card');
 const abano = document.getElementsByClassName('abano')
 const lenco = document.getElementsByClassName('lenco')
@@ -9,68 +14,102 @@ const btn_abanos = document.getElementById('abanos')
 const news = document.getElementById('news')
 const newsContainer = document.getElementsByClassName('news-container')[0];
 
-btn_todos.addEventListener('click',()=>{
+const theme = document.getElementById('theme')
+var tema = 0;
+// ############### EVENTS DEFINITION #################
 
-    btn_todos.style.color = '#EE8A9B'
-    btn_lencos.style.color = '#000'
-    btn_abanos.style.color = '#000'
-    
 
-    for(var i=0; i<=card.length; i++){
-        card[i].style.transition = '1s '
-        card[i].style.display= 'block'
+
+// ************ FILTER START *******************
+
+btn_todos.addEventListener('click', () => {
+
+    btn_todos.style.color = '#EE8A9B';
+    btn_lencos.style.color = '#000';
+    btn_abanos.style.color = '#000';
+
+
+    for (var i = 0; i <= card.length; i++) {
+        card[i].style.transition = '0.5s ';
+        card[i].style.display = "block";
+
+        setTimeout(() => {
+
+            for (var i = 0; i <= card.length; i++) {
+                card[i].style.opacity = 1;
+                card[i].style.transform = "scale(1)";
+            }
+
+        }, 200);
+
     }
 })
 
-btn_lencos.addEventListener('click',()=>{
+btn_lencos.addEventListener('click', () => {
 
     btn_todos.style.color = '#000'
     btn_lencos.style.color = '#EE8A9B'
     btn_abanos.style.color = '#000'
 
-    for(let i=0; i<lenco.length; i++){
+    for (let i = 0; i < lenco.length; i++) {
+        lenco[i].style.transition = '0.5s';
+        lenco[i].style.display = 'block';
 
-        lenco[i].style.translet = '1s '
-        lenco[i].style.display = 'block'
+        setTimeout(() => {
+            lenco[i].style.opacity = 1;
+            lenco[i].style.transform = "scale(1)";
+
+        }, 600);
+
     }
 
-    for(let i=0; i<abano.length; i++){
-        
-        abano[i].style.display = 'none'
+    for (let i = 0; i < abano.length; i++) {
+        abano[i].style.transform = "scale(0.6)";
+        abano[i].style.opacity = 0;
+
+        setTimeout(() => {
+            abano[i].style.display = 'none'
+        }, 500);
     }
 })
 
-btn_abanos.addEventListener('click',()=>{
+btn_abanos.addEventListener('click', () => {
 
     btn_todos.style.color = '#000'
     btn_lencos.style.color = '#000'
     btn_abanos.style.color = '#EE8A9B'
 
-    for(let i=0; i<abano.length; i++){
-        abano[i].style.transition = '1s '
+    for (let i = 0; i < abano.length; i++) {
+        abano[i].style.transition = '0.5s '
         abano[i].style.display = 'block'
+
+        setTimeout(() => {
+            abano[i].style.opacity = 1;
+            abano[i].style.transform = "scale(1)";
+        }, 600);
     }
 
-    for(let i=0; i<lenco.length; i++){
-        lenco[i].style.transition = '1s'
-        lenco[i].style.display = 'none'
+    for (let i = 0; i < lenco.length; i++) {
+        lenco[i].style.transform = "scale(0.6)";
+        lenco[i].style.opacity = 0;
+
+        setTimeout(() => {
+            lenco[i].style.display = 'none';
+        }, 500);
 
     }
 })
 
+const verticalShow = document.getElementById('vertical-display')
 
-// ############## NEWS PRODUCT DISPLAY MODE ##################
-
- const verticalShow = document.getElementById('vertical-display')
-
- verticalShow.addEventListener('click', ()=>{
+verticalShow.addEventListener('click', () => {
     verticalShow.style.color = '#EE8A9B'
     horizontalShow.style.color = '#000'
 
     news.style.maxHeight = 'auto'
 
-    
-    for(let i=0; i< card.length; i++){
+
+    for (let i = 0; i < card.length; i++) {
         card[i].style.minHeight = ' 360px'
         card[i].style.minWidth = ' 280px'
         card[i].style.marginTop = ' 1rem'
@@ -85,16 +124,16 @@ btn_abanos.addEventListener('click',()=>{
     newsContainer.style.marginBotton = '2rem'
 
 
- })
+})
 
- const horizontalShow = document.getElementById('horizontal-display')
+const horizontalShow = document.getElementById('horizontal-display')
 
- horizontalShow.addEventListener('click', ()=>{
+horizontalShow.addEventListener('click', () => {
 
     verticalShow.style.color = '#000'
     horizontalShow.style.color = '#EE8A9B'
 
-    for(let i=0; i< card.length; i++){
+    for (let i = 0; i < card.length; i++) {
         card[i].style.minHeight = ' 300px'
         card[i].style.minWidth = ' 220px'
         card[i].style.marginTop = ' 0'
@@ -105,32 +144,98 @@ btn_abanos.addEventListener('click',()=>{
     newsContainer.style.width = '100%'
     newsContainer.style.marginBotton = '0.5rem'
 
-    setTimeout(()=>{
+    setTimeout(() => {
         newsContainer.style.height = '340px'
     }, 900)
 
- })
+})
+
+// ************ FILTER END *******************
 
 
- const preloader = document.querySelector('#preloader')
+// ************ PRELOADER *******************
 
- window.addEventListener('load', ()=>{
+const preloader = document.getElementsByClassName('preloader')
 
-    setTimeout(()=>{
-        preloader.style.display ='none'
+window.addEventListener('load', () => {
+
+    setTimeout(() => {
+        for (preload of preloader) {
+            preload.style.display = 'none'
+        }
     }, 2000)
 
- })
-
-
-//  ##############3 THEME ###################
-
-const theme = document.getElementById('theme')
-
-theme.addEventListener('click', ()=>{
-    const body = document.getElementsByTagName('body')[0]
-    const headerH1 = document.querySelector('.hdr-content h1')
-
-    body.style.background = 'linear-gradient(60deg, #31002b, #240120)'
-    headerH1.style.color ='#fff'
 })
+
+
+// ************ THEME DEFINITION START *******************
+
+
+
+theme.addEventListener('click', tehme1)
+
+
+// ************ THEME DEFINITION END *******************
+
+
+
+// ########## FUNCTIONS  ################
+
+function tehme1(){
+    const body = document.getElementsByTagName('body')[0];
+    const headerH1 = document.querySelector('.hdr-content h1');
+    const logoTheme = document.querySelector('.logo-theme');
+    const filter = document.querySelectorAll('.filter button');
+    const filterContainer = document.querySelectorAll('.filter')[0];
+    const lencoShowCaer = document.querySelectorAll('.lencos-card img');
+
+    body.style.background = 'linear-gradient(60deg, #31002b, #240120)';
+    headerH1.style.color = '#fff';
+    logoTheme.style.background = 'linear-gradient(60deg, #31002b, #240120)';
+    logoTheme.style.boxShadow = 'none';
+
+    for (let i = 0; i < filter.length; i++) {
+        filter[i].style.color = 'white';
+    }
+
+    filterContainer.style.boxShadow = "1px 1px 10px #222";
+    
+    for (let i = 0; i < card.length; i++) {
+        card[i].style.boxShadow = '3px 4px 10px #333';
+    }
+
+    
+
+
+
+
+} // THEME1 End
+
+
+function addHorizontalShow(verticalColor, horizontalColor) {
+    verticalShow.style.color = verticalColor
+    horizontalShow.style.color = horizontalColor
+
+    for (let i = 0; i < card.length; i++) {
+        card[i].style.minHeight = ' 300px'
+        card[i].style.minWidth = ' 220px'
+        card[i].style.marginTop = ' 0'
+        card[i].style.transition = ' 1s ease-in-out'
+    }
+
+    newsContainer.style.flexDirection = 'row'
+    newsContainer.style.width = '100%'
+    newsContainer.style.marginBotton = '0.5rem'
+
+    setTimeout(() => {
+        newsContainer.style.height = '340px'
+    }, 900)
+}
+
+// ########## PRELOADER  ################
+
+
+
+
+
+
